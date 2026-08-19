1d1bd94270501bb90b34d0cf128998b12dfde98c
Duncan <mofiamadiback@gmail.com>
2026-08-19 12:08:20 +0300

    update product.py

--- models/product.py
+++ models/product.py
@@ -2,4 +2,14 @@ from datetime import datetime
 from db import db #importing 'shared SQLAlchemy
 
 class Supplier(db.Model):
-    __tablename__ = 
+    __tablename__ = 'suppliers'
+
+    supplier_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=True)
+    email = db.Column(db.string(120), nullable=True)
+    address = db.Column(db.String(255), nullable=True)
+
+    #   Relationship to products supplied 
+    products = db.relationship('Product', backref='supplier', lazy=True)
+    

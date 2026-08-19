e3def92ee4fe89b87ffd68fff4d94273cacc44c3
cynthia <njugunacynthia32@gmail.com>
2026-08-19 14:45:25 +0300

    add store_admin.py

---
+++ models/store_admin.py
@@ -0,0 +1,23 @@
+from extensions import db
+
+
+class StoreAdmin(db.Model):
+    __tablename__ = "store_admins"
+
+    admin_id = db.Column(db.Integer, primary_key=True)
+    admin_name = db.Column(db.String(100), nullable=False)
+
+    merchant_id = db.Column(
+        db.Integer,
+        db.ForeignKey("merchants.merchant_id"),
+        nullable=False
+    )
+
+    store_id = db.Column(
+        db.Integer,
+        db.ForeignKey("stores.store_id"),
+        nullable=False
+    )
+
+    def __repr__(self):
+        return f"<StoreAdmin {self.admin_name}>"

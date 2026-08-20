6eb0990b2cea6dc16bb81b4f38409879d929fc2c
cynthia <njugunacynthia32@gmail.com>
2026-08-20 18:47:29 +0300

    add schemas

---
+++ schemas/store_admin_schema.py
@@ -0,0 +1,18 @@
+from extensions import ma
+from models.store_admin import StoreAdmin
+
+
+class StoreAdminSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = StoreAdmin
+        load_instance = True
+        include_fk = True
+
+    admin_id = ma.auto_field()
+    admin_name = ma.auto_field()
+    merchant_id = ma.auto_field()
+    store_id = ma.auto_field()
+
+
+store_admin_schema = StoreAdminSchema()
+store_admins_schema = StoreAdminSchema(many=True)

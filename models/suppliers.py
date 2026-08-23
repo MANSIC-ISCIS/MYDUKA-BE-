d97a43b0c7bb55a3f8b105e192c80d83c38d7b03
Winkesa <winfreywekesa148@gmail.com>
2026-08-23 13:49:55 +0300

    feat: Refactor SQLAlchemy import and remove products relationship
    Updated the import statement for the SQLAlchemy instance from 'db' to 'extensions'. Removed the relationship definition for products supplied.

--- models/suppliers.py
+++ models/suppliers.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from db import db  # Importing shared SQLAlchemy instance
+from extensions import db  # Importing shared SQLAlchemy instance
 
 class Supplier(db.Model):
     __tablename__ = 'suppliers'
@@ -9,6 +9,3 @@ class Supplier(db.Model):
     phone_number = db.Column(db.String(20), nullable=True)
     email = db.Column(db.String(120), nullable=True)
     address = db.Column(db.String(255), nullable=True)
-
-    # Relationship to products supplied
-    products = db.relationship('Product', backref='supplier', lazy=True)
